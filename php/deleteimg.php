d7316681489771cd016ab347bbbaa4172b68d435
Immanuel <57101108+immanuel404@users.noreply.github.com>
2021-02-23 12:33:11 +0200

    Add files via upload

---
+++ php/deleteimg.php
@@ -0,0 +1,13 @@
+<?php
+require 'connect.php';
+
+
+$id = $_GET['id'];
+
+$sql = "DELETE FROM `images` WHERE `file_name` = '{$id}' LIMIT 1 ";
+
+if(mysqli_query($con, $sql)) {
+	http_response_code(204);
+} else {
+	http_response_code(422);
+}
