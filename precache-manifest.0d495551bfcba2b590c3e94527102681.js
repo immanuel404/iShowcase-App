1e7339b424a7154e58867563dde8e2f05eac72b2
Immanuel <57101108+immanuel404@users.noreply.github.com>
2021-12-01 13:35:27 +0200

    Add files via upload

---
+++ precache-manifest.0d495551bfcba2b590c3e94527102681.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e177cb258d7f961e39241cb7afd4221",
+    "url": "/iShowcase-App/index.html"
+  },
+  {
+    "revision": "d29a450553555faa93d9",
+    "url": "/iShowcase-App/static/css/main.796f5faf.chunk.css"
+  },
+  {
+    "revision": "6da3121998267b7eb6e6",
+    "url": "/iShowcase-App/static/js/2.79a5d9b1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/iShowcase-App/static/js/2.79a5d9b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d29a450553555faa93d9",
+    "url": "/iShowcase-App/static/js/main.68773717.chunk.js"
+  },
+  {
+    "revision": "e673b7741c264dfa6498",
+    "url": "/iShowcase-App/static/js/runtime-main.5094d9c0.js"
+  },
+  {
+    "revision": "c07a54cd8ebe7dbab5fd6f02e28b568c",
+    "url": "/iShowcase-App/static/media/comingsoon.c07a54cd.png"
+  },
+  {
+    "revision": "20b8dd1a88adebaf0573da98cb362e8a",
+    "url": "/iShowcase-App/static/media/undraw.20b8dd1a.svg"
+  },
+  {
+    "revision": "f17dcc647a3ff4898660f61f4307bbfe",
+    "url": "/iShowcase-App/static/media/undraw2.f17dcc64.svg"
+  }
+]);
