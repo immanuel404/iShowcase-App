7e9949105dcb8ac9a7225b42e6547a825aee5b4f
Emmanuel <57101108+immanuel404@users.noreply.github.com>
2022-05-18 22:34:33 +0200

    Add files via upload

---
+++ precache-manifest.e2eb825c392388198c8ee7f76e696693.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f07defac547d09c6553fd922d9d3e180",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f6e36269634e74247c9",
+    "url": "/static/css/main.3bc8b30f.chunk.css"
+  },
+  {
+    "revision": "13e847f9cd64043ab912",
+    "url": "/static/js/2.837f71cb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.837f71cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f6e36269634e74247c9",
+    "url": "/static/js/main.43fa31a5.chunk.js"
+  },
+  {
+    "revision": "906af693db955c6952f6",
+    "url": "/static/js/runtime-main.a8908d5b.js"
+  },
+  {
+    "revision": "ee3382bc8c7636ae18f190472f064bc2",
+    "url": "/static/media/camera.ee3382bc.jpg"
+  },
+  {
+    "revision": "c07a54cd8ebe7dbab5fd6f02e28b568c",
+    "url": "/static/media/comingsoon.c07a54cd.png"
+  },
+  {
+    "revision": "df02bfd4af340c18d5c25240372a0bc1",
+    "url": "/static/media/laptop1.df02bfd4.jpg"
+  },
+  {
+    "revision": "6af8fa8ee38ed041a7c8cd4672e32c52",
+    "url": "/static/media/turntable.6af8fa8e.jpg"
+  },
+  {
+    "revision": "20b8dd1a88adebaf0573da98cb362e8a",
+    "url": "/static/media/undraw.20b8dd1a.svg"
+  },
+  {
+    "revision": "f17dcc647a3ff4898660f61f4307bbfe",
+    "url": "/static/media/undraw2.f17dcc64.svg"
+  }
+]);
